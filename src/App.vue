1f9c5bd791aa4e6b47ac3b459ce59fd04af181f0
layla7 <laylamouat04@gmail.com>
2026-02-26 02:29:28 +0000

    fixed it :)

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,13 @@
 <script setup></script>
 
 <template>
-  <div class="bg-image"></div>
-
-  <div class = "bd-example pt-2 pb-5 px-5">
-    <div class="card text-center mb-3">
-  <h1 class="card-header">The Banana Institute</h1>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <h6 class="card-subtitle text-muted">Support card subtitle</h6>
-  </div>
-  <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
-    <rect width="100%" height="100%" fill="#868e96"></rect>
-    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
-  </svg>
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-  <div class="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
-<div class="card">
-  <div class="card-body">
-    <h4 class="card-title">Card title</h4>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+  <div>
+    <img class= "img-fluid w-100" src="/Banana Institute.png" alt="Banana Institute">
   </div>
 
-  <img src="/assets/bi-background.png" alt="">
+  
 </template>
 
 <style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '/assets/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
